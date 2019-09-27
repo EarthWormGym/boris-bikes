8d22dbb4d3927f97ce3e3434e3309373a53aa877
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-09-27 14:52:33 +0100

    update

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -19,4 +19,20 @@ describe DockingStation do
   end
 
   it { is_expected.to respond_to(:bike) }
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+    end
+  end
+  describe '#release_bike' do
+  it 'raises an error when there are no bikes available' do
+    # Let's not dock a bike first:
+    # remember subject == DockingStation.new
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end 
 end
